71fb3888616d7b9ac901bd1d4a33fb15c0a97e3e
adessa73 <74878806+adessa73@users.noreply.github.com>
2024-04-12 10:26:17 +0100

    Update DetailField.vue

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,43 +1,45 @@
 <template>
     <panel-item :index="index" :field="fieldLabel" />
-</template>
-
-<script>
-    let vm;
-
-    export default {
-        data() {
-            return {
-                tags: '',
-                fieldLabel: {},
-                tagsWrapperClass: 'nti-tags-wrapper',
-                tagClass: 'nti-tag',
-            }
-        },
-        props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
-        mounted() {
-            // define this, provides access to the scope inside the forEach
-            vm = this;
-            // clone the original field prop
-            this.fieldLabel = { ...this.field };
-            // set the label the same as the original field prop
-            this.fieldLabel.name = this.field.name;
-            // get the value array
-            let items = this.field.value;
-            if (items instanceof Array && items.length) {
-                // loop through the tags
-                items.forEach(function (item, index) {
-                    // Backward compatibility in case tags are stored as object
-                    let label = (typeof item === "object" && item.hasOwnProperty('text')) ? item.text : item;
-                    vm.tags += '<span class="' + vm.tagClass + '">' + label + '</span>';
-                });
-                // wrap the value
-                this.fieldLabel.value = '<div class="' + this.tagsWrapperClass + '">' + this.tags + '</div>';
-                // set the label as html
-                this.fieldLabel.asHtml = true;
-            } else {
-                this.fieldLabel.value = '—';
-            }
+  </template>
+  
+  <script>
+  let vm;
+  
+  export default {
+    data() {
+      return {
+        tags: '',
+        fieldLabel: {},
+        tagsWrapperClass: 'nti-tags-wrapper',
+        tagClass: 'nti-tag',
+      };
+    },
+    props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+    mounted() {
+      vm = this;
+      this.fieldLabel = { ...this.field };
+      let items = this.field.value;
+      if (typeof items === 'string') {
+        // Se os itens são uma string, vamos tentar analisá-los como JSON
+        try {
+          items = JSON.parse(items);
+        } catch (e) {
+          // Em caso de erro na análise JSON, trate como uma string simples
+          items = [items];
         }
-    }
-</script>
+      }
+      if (items instanceof Array && items.length) {
+        items.forEach(function (item, index) {
+          // Backward compatibility in case tags are stored as object
+          let label = (typeof item === 'object' && item.hasOwnProperty('text')) ? item.text : item;
+          vm.tags += '<span class="' + vm.tagClass + '">' + label + '</span>';
+        });
+        this.fieldLabel.value = '<div class="' + this.tagsWrapperClass + '">' + vm.tags + '</div>';
+        // Set the label as HTML
+        this.fieldLabel.asHtml = true;
+      } else {
+        this.fieldLabel.value = '—';
+      }
+    },
+  };
+  </script>
